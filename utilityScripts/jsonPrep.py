79003ffe6829ac38a19159fb0d1c5e74269eae39
smithbatchelec <111441168+smithbatchelec@users.noreply.github.com>
2022-10-01 13:57:47 +0530

    allay console erros fixed

--- utilityScripts/jsonPrep.py
+++ utilityScripts/jsonPrep.py
@@ -10,6 +10,23 @@ with open("/Users/erdemisbilen/Angular/fashionWebScraping/csvFiles/jsonFiles.csv
 	for row in reader:
 
 		jsonFile=row['jsonFile_raw']
+  productName=Field()
+    priceOriginal=Field()
+    priceSale=Field()
+
+    #items to store links 
+    imageLink = Field()
+    productLink=Field()
+
+    #item for company name
+    company = Field()
+
+    #items for image pipeline
+    #image_urls = scrapy.Field()
+    #images = scrapy.Field()
+
+
+    pass
 
 		with open(jsonFile) as json_file:
 
