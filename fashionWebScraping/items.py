cd1524bd716ea39dc5fe0d39eaf60fab87f1ac6d
smithbatchelec <111441168+smithbatchelec@users.noreply.github.com>
2022-10-01 13:57:06 +0530

    items controller python imp

--- fashionWebScraping/items.py
+++ fashionWebScraping/items.py
@@ -28,3 +28,20 @@ class FashionwebscrapingItem(scrapy.Item):
 class ImgData(Item):
     image_urls=scrapy.Field()
     images=scrapy.Field()
+  productName=Field()
+    priceOriginal=Field()
+    priceSale=Field()
+
+    #items to store links 
+    imageLink = Field()
+    productLink=Field()
+
+    #item for company name
+    company = Field()
+
+    #items for image pipeline
+    #image_urls = scrapy.Field()
+    #images = scrapy.Field()
+
+
+    pass
